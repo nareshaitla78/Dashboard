a6c77b984cc0db8cd908a5578c7d46459713ef05
nareshaitla78 <nareshaitla1122@gmail.com>
2025-03-07 23:38:17 +0530

    Done with basic setup with vue 3 and tailwind css and pinia store

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Tailwind CSS with Vue 3 Options API!",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="h-screen flex flex-col justify-center items-center bg-blue-500 text-white text-3xl">
+    <h1>{{ message }}</h1>
+    <button 
+      class="mt-4 px-6 py-2 bg-white text-blue-500 rounded-lg hover:bg-gray-200 transition">
+      Click Me
+    </button>
+  </div>
+</template>
